07af852b966b8ea0d0293d3fc8dba0a8a6a979e5
chapter 4 <lopeznavarroalex5@gmail.com>
2023-02-15 12:29:26 +0100

    Commit codigo con el metodo generarPrimos refactorizado

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
         int dato=teclado.nextInt();
-        int vector[]=new int[dato];
+        int[] vector =new int[dato];
         System.out.println("\nVector inicial hasta :"+dato);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
@@ -32,29 +32,47 @@ public class Main {
             // Eliminar el 0 y el 1, que no son primos
             esPrimo[0] = esPrimo[1] = false;
             // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j=2*i; j<dim; j+=i)
-                        esPrimo[j] = false;
-                }
-            }
+            criba(dim, esPrimo);
             // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i=0; i<dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
+            int cuenta = contarPrimos(dim, esPrimo);
             // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i=0, j=0; i<dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
-            return primos;
+            return rellenaVectorNumerosPrimos(dim, esPrimo, cuenta);
         } else { // max < 2
             return new int[0];
         // Vector vacío
         }
+    }
+
+    private static void criba(int dim, boolean[] esPrimo) {
+        int j;
+        int i;
+        for (i=2; i<Math.sqrt(dim)+1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (j=2*i; j< dim; j+=i)
+                    esPrimo[j] = false;
+            }
+        }
+    }
+
+    private static int contarPrimos(int dim, boolean[] esPrimo) {
+        int i;
+        int cuenta = 0;
+        for (i=0; i< dim; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+
+    private static int[] rellenaVectorNumerosPrimos(int dim, boolean[] esPrimo, int cuenta) {
+        int j;
+        int i;
+        int[] primos = new int[cuenta];
+        for (i=0, j=0; i< dim; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
     }
 }
