5f5b92701c2abfdb8f81038180de9538fd139b90
chapter 4 <lopeznavarroalex5@gmail.com>
2023-02-17 12:30:20 +0100

    Commit con todo el documentado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * En este metodo se inicializan y se ejecutan los datos para realizar la criba de Erastotenes
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
@@ -13,6 +17,10 @@ public class Main {
         vectorPrimos(vector);
     }
 
+    /**
+     * El metodo lo que hace es calcular e imprimir los numeros del vector que se le pasa. Cuando llega a un numero multiplo de 10 imprime un salto de linia.
+     * @param vector es un vector de datos que le pasamos al metodo
+     */
     private static void vectorPrimos(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
@@ -20,6 +28,10 @@ public class Main {
         }
     }
 
+    /**
+     * Imprime los numeros desde el 1 hasta el numero que se le pasa para la criba
+     * @param vector es un vector de datos que le pasamos al metodo, definido en el main
+     */
     private static void vectorInicial(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
@@ -28,6 +40,12 @@ public class Main {
     }
 
     // Generar números primos de 1 a max
+
+    /**
+     * Este metodo lo que hace es calcular los numeros primos hasta un numero maximo que se le pasa al metodo.
+     * @param max es el maximo de numeros primos que vamos a generar
+     * @return devuelve un vector de numeros primos
+     */
     public static int[] generarPrimos (int max)
     {
         int i,j;
@@ -52,6 +70,11 @@ public class Main {
         }
     }
 
+    /**
+     * Este metodo lo que realiza es la criba de Erastotenes
+     * @param dim es el tamaño del array que creamos en el main + 1
+     * @param esPrimo es un vector de booleanos que nos indica si el numero que hay en esa posicion es primo o no es primo
+     */
     private static void criba(int dim, boolean[] esPrimo) {
         int j;
         int i;
@@ -64,6 +87,12 @@ public class Main {
         }
     }
 
+    /**
+     * El metodo cuenta la cantidad de numeros primos que hay
+     * @param dim es el tamaño del array que creamos en el main + 1
+     * @param esPrimo es un vector de booleanos que nos indica si el numero que hay en esa posicion es primo o no es primo
+     * @return devuelve un entero con la cantidad de numeros primos que hay en el vector
+     */
     private static int contarPrimos(int dim, boolean[] esPrimo) {
         int i;
         int cuenta = 0;
@@ -74,6 +103,13 @@ public class Main {
         return cuenta;
     }
 
+    /**
+     * El metodo lo que hace es rellenar el vector con aquellos numeros que son primos
+     * @param dim es el tamaño del array que creamos en el main + 1
+     * @param esPrimo es un vector de booleanos que nos indica si el numero que hay en esa posicion es primo o no es primo
+     * @param cuenta es la cantidad de numeros primos que hay
+     * @return devuelve un vector con los numeros primos
+     */
     private static int[] rellenaVectorNumerosPrimos(int dim, boolean[] esPrimo, int cuenta) {
         int j;
         int i;
